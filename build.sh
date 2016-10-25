ec35043d4b187044059affc7a917b690584d0ca7
Gosselin Jean-Baptiste <gosselinjb@gmail.com>
2016-10-25 17:41:16 +0200

    add latest build

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ IMAGE_NAME="gosselj/meteor"
 TAG_1_3="1.3"
 ALIAS_TAG_1_3="1.3.5 1.3.5.1"
 TAG_1_4="1.4"
-ALIAS_TAG_1_4="1.4.1 1.4.1.3 latest"
+ALIAS_TAG_1_4="1.4.1 1.4.1.3"
 
 docker build -t ${IMAGE_NAME}:${TAG_1_3} ./1.3/
 docker push ${IMAGE_NAME}:${TAG_1_3}
@@ -22,3 +22,7 @@ for tag in ${ALIAS_TAG_1_4}; do
 	docker push ${IMAGE_NAME}:${tag}
 	docker rmi ${IMAGE_NAME}:${tag}
 done
+
+docker build -t ${IMAGE_NAME}:latest ./latest/
+docker push ${IMAGE_NAME}:latest
+docker rmi ${IMAGE_NAME}:latest
